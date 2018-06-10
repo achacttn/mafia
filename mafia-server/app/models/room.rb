ec801006994ccc20297915406f8d2357c0b7e61d
Ansatasia Gorodinskaya <agorodinskaya@gmail.com>
2018-06-10 16:55:21 +1000

    rooms validation

--- mafia-server/app/models/room.rb
+++ mafia-server/app/models/room.rb
@@ -2,4 +2,7 @@ class Room < ApplicationRecord
     has_many :users
     has_many :gameStates, through: :users
     validates :name,:presence => true
+    validates :name,:length => { :minimum => 2 }
+    validates :name,uniqueness: true
+
 end
