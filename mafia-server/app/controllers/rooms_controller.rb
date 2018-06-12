0276cde04b855f721478c17cf32e93c5fbe9d485
Scott Lee <achacttn@gmail.com>
2018-06-12 19:24:23 +1000

    game logic

--- mafia-server/app/controllers/rooms_controller.rb
+++ mafia-server/app/controllers/rooms_controller.rb
@@ -30,8 +30,7 @@ class RoomsController < ApplicationController
     @message = Message.new
     @current_user.update( room_id: params[:id] )
 
-    
-
+  
     if @room.users.length > 2
       @room.update(gamestate: {
         canStart: true,
@@ -43,8 +42,79 @@ class RoomsController < ApplicationController
         hasStarted: false
       })
     end
+  
+    if @room.gamestate[:hasStarted]
+      @room.users.sample((@room.users.length/3).floor).each do |player|
+        player.stateobject[:mafia] = true
+      end
+      
+      @room.users.each do |p|
+        if !p.stateobject[:mafia]
+          p.stateobject[:mafia] = false
+        end
+      end
+    end
+
+    livingMafia = []
+    livingCitizens = []
+    @room.users.each do |p|
+      if p.stateobject[:alive]
+        if p.stateobject[:mafia]
+          livingMafia.push(p)
+        else
+          livingCitizens.push(o)
+        end
+      end
+    end
     
+    def endingCheck()
+      # check number of livng mafia and living citizens
+      if livingMafia.length == 0
+        return true
+      elsif livingMafia.length > livingCitizens.length
+        return true
+      elsif livingMafia.length == 1 && livingCitizens.length == 1
+        return true
+      else
+        return false
+      end
+    end
+
+    def dayTimer()
+      timeTilDayEnd = 120
+      while (timeTilDayEnd != 0)
+        puts timeTilDayEnd
+        sleep 1
+        timeTilDayEnd = timeTilDayEnd-1
+      end
+    end
+
+    def nightTimer()
+      timeTilNightEnd = 30
+      while (timeTilNightEnd != 0)
+        puts timeTilNightEnd
+        sleep 1
+        timeTilNightEnd = timeTilNightEnd-1
+    end
+    
+    dayOrNight = -1
+    
+    loop do
+
+      if (endingCheck())
+        break
+      end
+
+      if dayOrNight > 0
+        dayTimer()
+      else
+        nightTimer()
+      end
+      dayOrNight *= -1
+    end
+
   end
+
 
   def broadcast
     ActionCable.server.broadcast 'messages',
