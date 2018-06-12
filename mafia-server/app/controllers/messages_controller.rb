4e2ecf623bd80dfffd955dfdfaaa2cdae138ddc5
Linna Liu <cello1412@gmail.com>
2018-06-12 10:45:09 +1000

    ActionCable fixed

--- mafia-server/app/controllers/messages_controller.rb
+++ mafia-server/app/controllers/messages_controller.rb
@@ -1,31 +1,27 @@
 class MessagesController < ApplicationController
   before_action :check_if_logged_in
 
-  # def broadcast
-  #   ActionCable.server.broadcast 'messages',
-  #     message: @message.text_body,
-  #     user: @message.user.name
-  #     head :ok
-  # end
-
   def create
-    @message = Message.new
-    @message.user_id = @current_user.id
-    @message.text_body = params[:text_body]
-    @message.room_id = params[:room_id]
-    @message.save
 
-    if @message.persisted?
+    @message = Message.new message_params
+    @message.user = @current_user
+
+    if @message.save
       # render :broadcast
+
+      p @message.text_body
+      p @message.user
+
       ActionCable.server.broadcast 'messages',
         message: @message.text_body,
         user: @message.user.name
         head :ok
-
+    # else
+    #   binding.pry
     end
     #
     # raise "hell"
-      redirect_to room_path params[:room_id] and return
+      # redirect_to room_path params[:room_id] and return
 
     # if message.persisted?
     #   redirect_to room_path params[:room_id]
